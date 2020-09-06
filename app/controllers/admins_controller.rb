7c14a0d17b547335fe6b4d20d31c45bdf5f816ab
jirou6699 <shundom09@gmail.com>
2020-09-06 23:00:13 +0900

    admin-configration

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,20 @@
 class AdminsController < ApplicationController
   def index
-    @jobs = Job.new
+    @jobs = Job.all
   end
-end
+  
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :image1, :image2, :image3, :image4, :image5, :description, :salary, :access, :time, :week, :employee, :term)
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'chats/index'
   root 'tops#index'
   resources :tops, only: [:show, :new, :create]
   resources :chats, only: [:index, :create]
